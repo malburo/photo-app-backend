feab37d3858da150893b8b139936edd0e4448fd3
malburo <tongquocbao0605@gmail.com>
2021-01-12 12:58:06 +0700

    create master router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,14 @@
-import AuthRouter from "../modules/Auth/auth.route";
+import checkToken from '../middlewares/token.middleware';
+import AuthRouter from '../modules/Auth/auth.route';
+import PhotoRouter from '../modules/Photo/photo.route';
+import UploadRouter from '../modules/Upload/upload.route';
+import UserRouter from '../modules/User/user.route';
 
 const MasterRouter = (app) => {
-  app.use("/api/auth", AuthRouter);
+  app.use('/api/auth', AuthRouter);
+  app.use('/api/photos', PhotoRouter);
+  app.use('/api/users', checkToken, UserRouter);
+  app.use('/api/upload', checkToken, UploadRouter);
 };
 
 export default MasterRouter;
