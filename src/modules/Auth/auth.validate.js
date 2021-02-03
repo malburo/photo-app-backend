599fae44357dc36a9f1520505fa3be538b1b40a7
malburo <tongquocbao0605@gmail.com>
2021-02-03 20:40:39 +0700

    validate

--- src/modules/Auth/auth.validate.js
+++ src/modules/Auth/auth.validate.js
@@ -1,10 +1,46 @@
 import Result from '../../helpers/result.helper';
 
+function checkEmail(email) {
+  const filter = /(\W|^)[\w.+\-]*@gmail\.com(\W|$)/;
+  if (!filter.test(email)) {
+    return false;
+  }
+  return true;
+}
+
 function validateFormRegister(req, res, next) {
   try {
-    const { fullname, email, password } = req.body;
-    if ((fullname === '' || email === '', password === '')) {
-      return Result.error(res, { message: 'Không được để trống' });
+    const { fullname, email, password, retypePassword } = req.body;
+    if (fullname === '' || email === '' || password === '' || retypePassword === '')
+      return Result.error(res, { message: 'Không được để trống!' });
+
+    if (fullname.length < 2 || fullname.length > 30) {
+      return Result.error(res, { message: 'Họ tên phải có độ dài từ 2-30 ký tự!' });
+    }
+
+    if (password !== retypePassword)
+      return Result.error(res, { message: 'Xác nhận mật khẩu không đúng với mật khẩu!' });
+
+    if (checkEmail(email) === false)
+      return Result.error(res, { message: 'Vui lòng nhập email đúng định dạng: abc@gmail.com' });
+    next();
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
+
+function validateFormLogin(req, res, next) {
+  try {
+    const { email, password } = req.body;
+    if (email === '' || password === '') {
+      return Result.error(res, { message: 'Vui lòng nhập đầy đủ tài khoản, mật khẩu và không được để trống!' });
+    }
+    if (email.length < 6 || email.length > 35) {
+      return Result.error(res, { message: 'Email phải từ 6-35 ký tự!' });
+    }
+    if (password.length < 6 || password.length > 30) {
+      return Result.error(res, { message: 'Mật khẩu phải là chuỗi có độ dài từ 6-30 ký tự!' });
     }
     next();
   } catch (error) {
@@ -12,4 +48,5 @@ function validateFormRegister(req, res, next) {
   }
 }
 
-export default validateFormRegister;
+const authValidate = { validateFormRegister, validateFormLogin, checkEmail };
+export default authValidate;
---
+++ src/modules/User/user.validate.js
@@ -0,0 +1,59 @@
+import Result from '../../helpers/result.helper';
+
+function checkEmail(email) {
+  const filter = /(\W|^)[\w.+\-]*@gmail\.com(\W|$)/;
+  if (!filter.test(email)) {
+    return false;
+  }
+  return true;
+}
+
+function UpdateInfoValidate(req, res, next) {
+  try {
+    const { fullname, email, bio } = req.body;
+
+    if (fullname === '' || email === '' || bio === '') {
+      return Result.error(res, { message: 'Không được để trống!' });
+    }
+    if (fullname.length < 2 || fullname.length > 30) {
+      return Result.error(res, { message: 'Họ tên có độ dài từ 2-30 ký tự!' });
+    }
+    if (email.length < 6 || email.length > 35) {
+      return Result.error(res, { message: 'Họ tên phải có độ dài từ 2-35 ký tự!' });
+    }
+    if (checkEmail(email) === false) {
+      return Result.error(res, { message: 'Vui lòng nhập email đúng định dạng: abc@gmail.com' });
+    }
+    if (bio.length < 2 || bio.length > 50) {
+      return Result.error(res, { message: 'Bio phải có độ dài từ 2-50 ký tự!' });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+function ChangePasswordValidate(req, res, next) {
+  try {
+    const { currentPassword, newPassword, retypePassword } = req.body;
+
+    if (currentPassword === '' || newPassword === '' || retypePassword === '') {
+      return Result.error(res, { message: 'Không được để trống!' });
+    }
+    if (currentPassword.length < 6 || currentPassword.length > 30) {
+      return Result.error(res, { message: 'Password phải có độ dài từ 6-30 ký tự!' });
+    }
+    if (newPassword.length < 6 || newPassword.length > 30) {
+      return Result.error(res, { message: 'Password mới phải có độ dài từ 6-30 ký tự!' });
+    }
+    if (retypePassword !== newPassword) {
+      return Result.error(res, { message: 'Xác nhận mật khẩu không đúng với mật khẩu!' });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+const UserValidate = { UpdateInfoValidate, ChangePasswordValidate };
+export default UserValidate;
