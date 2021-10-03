116b05a9d234c222dfb7b96b511fb642b930317b
malburo <tongquocbao0605@gmail.com>
2021-10-03 12:52:05 +0700

    update mongo uri

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/', {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
