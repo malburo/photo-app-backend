ad0256ef48f70d47b8d360dd0834777b226bcef1
malburo <tongquocbao0605@gmail.com>
2021-10-23 19:05:28 +0700

    validate feature

--- src/modules/Auth/auth.route.js
+++ src/modules/Auth/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import checkToken from 'middlewares/token.middleware';
 import authController from './auth.controller';
+import authValidate from './auth.validate';
 const AuthRouter = express.Router();
 
 AuthRouter.route('/my-profile').get(checkToken, authController.getMe);
-AuthRouter.route('/login').post(authController.login);
-AuthRouter.route('/register').post(authController.register);
+AuthRouter.route('/login').post(authValidate.validateFormLogin, authController.login);
+AuthRouter.route('/register').post(authValidate.validateFormRegister, authController.register);
 export default AuthRouter;
